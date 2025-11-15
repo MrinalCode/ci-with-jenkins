0f4db04f192629e8cdac3daa187e274b00e14ae3
Ubuntu <ubuntu@ip-172-31-26-221.ec2.internal>
2025-11-15 14:24:23 +0000

    Updating BaseTest.java jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,28 +1,34 @@
 pipeline {
-    agent 
-     { label 'JDK17' } 
-   
+    agent { label 'JDK17' }
+
     stages {
 
-      stage('build the code'){
-        steps{
-          sh "mvn clean package"
+        stage('build the code') {
+            steps {
+                sh "mvn clean package"
+            }
         }
-      }
-      
-      stage('get the extent report'){
-        steps{
-         sh '''
-          'mkdir reports
-          'cp ./test-output/junitreports/* reports'
-        ''' 
-       }
-      }      
-    }  
-   
-    post { 
-        always { 
+
+        stage('Test') {
+            steps {
+                sh "mvn test"
+            }
+        } 
+
+        stage('get the extent report') {
+            steps {
+                sh '''
+                    mkdir -p reports
+                    cp -r test-output/* reports/ || true
+                '''
+            }
+        }
+    }
+
+    post {
+        always {
             echo 'I will always say Hello again!'
         }
-    } 
+    }
 }
+
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,31 +2,34 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class BaseTest {
 
     protected WebDriver driver;
-    //String browser = System.getProperty("browser");
 
-    @BeforeTest
+    @BeforeMethod
     public void setUp() {
-    
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
+        options.addArguments("--disable-software-rasterizer");
         options.addArguments("--remote-allow-origins=*");
 
         driver = new ChromeDriver(options);
-    }    
-   
+        driver.manage().window().setSize(new org.openqa.selenium.Dimension(1920, 1080));
+    }
 
-    @AfterTest
+    @AfterMethod
     public void tearDown() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
+
